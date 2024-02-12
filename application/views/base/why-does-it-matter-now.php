aef465f4631a400e859971faa2a33ed21b7d31e2
Devendra verma <88208166+KDevendra@users.noreply.github.com>
2024-02-12 18:24:53 +0530

    core file

--- application/views/base/why-does-it-matter-now.php
+++ application/views/base/why-does-it-matter-now.php
@@ -1,17 +1,84 @@
 <?php include_once __DIR__ . '/../common/header.php'; ?>
-<section class="about-section mt-150">
-    <div class="container-fluid">
-        <div class="about-wrapper gray-bg wow fadeInUp" style="visibility: visible;">
-            <div class="container">
-                <div class="row align-items-xl-center">
-                    <div class="col-xl-12 col-lg-12">
-                        <div class="about-three_content-box pl-lg-70 wow fadeInRight text-center" style="visibility: visible;">
-                            <div class="section-title mb-25 wow fadeInUp" style="visibility: visible;">
-                                <span class="sub-title">Website Under Construction</span>
-                                <h2>We're currently working on something awesome!</h2>
-                            </div>
-                            <p class="mb-40 wow fadeInDown" style="visibility: visible;">Thank you for visiting our website! We're excited to bring you an amazing new experience. Our team is hard at work behind the scenes to enhance your browsing experience and deliver fresh content.</p>
-                        </div>
+<section class="page-title-area text-white bg_cover mt___147" style="background-image: url('<?php echo base_url(); ?>include/web/custom/About_Banner.jpg');">
+    <div class="container">
+        <div class="page-title-inner text-center">
+            <h1 class="page-title">Why Does It Matter Now</h1>
+            <div class="gd-breadcrumb"><span class="breadcrumb-entry"><a href="<?php echo base_url(''); ?>">Home</a></span><span class="separator"></span><span class="breadcrumb-entry active">Why Does It Matter Now</span></div>
+        </div>
+    </div>
+</section>
+<section class="features-seciton pt-50 pb-0" id="matterNowDes">
+    <div class="container p-0">
+        <div class="row align-items-xl-end">
+            <div class="col-md-12">
+                <div class="section-title text-center mb-50 wow fadeInDown" style="visibility: visible; animation-name: fadeInDown;">
+                    <span class="sub-title"> <img src="<?php echo base_url(''); ?>include/web/custom/technology.png" alt=""> The Evolution of Digital Twin Technology: Why It Matters Now?</span>
+                    <h2>A Decade of Change in Data and Processing</h2>
+                </div>
+            </div>
+            <div class="col-lg-12 p-0">
+                <div class="features-image-box mb-20 wow fadeInLeft d-flex justify-content-center" style="visibility: visible;">
+                    <img src="<?php echo base_url(''); ?>include/web/custom/Matters_Now.png" width="100%" alt="Features Image">
+                </div>
+            </div>
+        </div>
+        <h4 class="text-center digi mt-20 mb-20">From 3D Modelling to full Digital Technology with AI-Driven Insight</h4>
+        <div class="row justify-content-center mt-80">
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-service-item-two mb-60 wow fadeInDown" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInDown;">
+                    <div class="icon"><img src="<?php echo base_url(''); ?>include/web/custom/3d-modellings-sh.png"></div>
+                    <div class="text">
+                        <h4 class="title"><a href="javascript:void(0)">The Origins in 3D Modelling: Setting the Foundations</a></h4>
+                        <h6 class="create"><a>From Concept to High-Fidelity 3D Modeling</a></h6>
+                        <p>Explore the journey of Digital Twins from their inception in the 1970s, starting with 3D modelling that offered cost-effective, high-fidelity replicas of physical entities, laying the groundwork for today's advanced applications.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-service-item-two mb-60 wow fadeInUp" data-wow-delay=".25s" style="visibility: visible; animation-delay: 0.25s; animation-name: fadeInUp;">
+                    <div class="icon"><img src="<?php echo base_url(''); ?>include/web/custom/i-o-t-sh.png"></div>
+                    <div class="text">
+                        <h4 class="title"><a href="javascript:void(0)">The IoT Revolution: Bringing the Real World into Digital</a></h4>
+                        <h6 class="create"><a>IoT: The Gateway to Real-World Data</a></h6>
+                        <p>Discover how the advent of IoT technology revolutionised Digital Twins, allowing for an unprecedented extraction of data and creating accurate replicas of real-world environments and systems
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-service-item-two mb-60 wow fadeInDown" data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInDown;">
+                    <div class="icon"><img src="<?php echo base_url(''); ?>include/web/custom/cloud-compute-sh.png"></div>
+                    <div class="text">
+                        <h4 class="title"><a href="javascript:void(0)">The Role of Cloud Computing: Scalability and Accessibility</a></h4>
+                        <h6 class="create"><a>Cloud Computing: The Backbone of Digital Twin Scalability</a></h6>
+                        <p>Understand how cloud computing has been instrumental in the evolution of Digital Twins, providing scalable storage and processing capabilities, thereby facilitating wider adoption and integration of IoT data.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-service-item-two mb-60 wow fadeInUp" data-wow-delay=".35s" style="visibility: visible; animation-delay: 0.35s; animation-name: fadeInUp;">
+                    <div class="icon"><img src="<?php echo base_url(''); ?>include/web/custom/ai-sh.png"></div>
+                    <div class="text">
+                        <h4 class="title"><a href="javascript:void(0)">The Rise of AI: Enhancing Perception and Decision-Making</a></h4>
+                        <h6 class="create"><a>AI: Elevating Awareness and Insights in Digital Twins</a></h6>
+                        <p>Delve into how AI technologies have expanded the capabilities of Digital Twins, raising our awareness and enhancing decision-making by processing complex data sets for more insightful outcomes.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-service-item-two mb-60 wow fadeInUp" data-wow-delay=".35s" style="visibility: visible; animation-delay: 0.35s; animation-name: fadeInUp;">
+                    <div class="icon"><img src="<?php echo base_url(''); ?>include/web/custom/community-sh.png"></div>
+                    <div class="text">
+                        <h4 class="title"><a href="javascript:void(0)">The Power of Community, Tools, and Standards: Accelerating
+                                Innovation</a>
+                        </h4>
+                        <h6 class="create"><a>Harnessing Collective Knowledge for Efficient Digital Twin
+                                Development</a>
+                        </h6>
+                        <p> Learn about the impact of the global tech community, open-source tools, and established standards in the Digital Twin arena. This collaborative environment enables the creation of scalable and interoperable digital replicas without the need for constant reinvention or costly software, propelling the field into a new era of innovation and practical application
+                        </p>
                     </div>
                 </div>
             </div>
